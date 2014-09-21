3c696b968ffdb7fbf74b112ea618914ac2394fa1
Dzenan Ridjanovic <dzenanr@gmail.com>
2014-09-21 11:06:13 +0200

    add dr.txt

--- ex02.dart
+++ ex02.dart
@@ -2,15 +2,15 @@ main (){
   num x = 5 ;
   num y = 7 ;
   num z = 8 ;
- print('Answer 1 : ${x-y/z} ');
- double pi = 3.14 ;
- num radius = 10 ;
- print ('Answer 2 : ${2*pi*radius}');
- num lotx = 220 ;
- num loty = 260 ;
- double acres = 43.560 ;
- print ('Answer 3 : The size of this lot in acres is ${lotx*loty/acres.round()}');
- num meters = 100 ;
- double seconds = 9.58 ; 
- print ('Answer 4 : Usain Bolt ran ${meters/seconds} m/s wheter ${(meters/1000)*(3600/9.580.round())}Km/h');
+  print('Answer 1 : ${x-y/z} ');
+  double pi = 3.14 ;
+  num radius = 10 ;
+  print ('Answer 2 : ${2*pi*radius}');
+  num lotx = 220 ;
+  num loty = 260 ;
+  double acres = 43.560 ;
+  print ('Answer 3 : The size of this lot in acres is ${lotx*loty/acres.round()}');
+  num meters = 100 ;
+  double seconds = 9.58 ; 
+  print ('Answer 4 : Usain Bolt ran ${meters/seconds} m/s wheter ${(meters/1000)*(3600/9.580.round())}Km/h');
 }
--- ex03.dart
+++ ex03.dart
@@ -1,11 +1,14 @@
 main() {
-      var s = '';
-      for (var i = 0; i <= 100; i++) {
-        s = '$s$i ';
-      }
-      print(s);
-      if (i%3 == 0){ print('Fizz'); } 
-    }
+  var s = '';
+  for (var i = 0; i <= 100; i++) {
+    s = '$s$i ';
+  }
+  print(s);
+  // undefined i
+  if (i%3 == 0) { 
+    print('Fizz'); 
+  } 
+}
   
   
   
