14242f4cc6b011b05e5e345ad79dac6ffbab138b
poumpoumtchak <dzenanr@gmail.com>
2014-09-21 21:32:39 -0400

    Revert "Revert "add ex03""
    This reverts commit b5948e4c7016e0a3da3253ae40e75582df230699.

---
+++ ex03.dart
@@ -0,0 +1,14 @@
+main() {
+      var s = '';
+      for (var i = 0; i <= 100; i++) {
+        s = '$s$i ';
+      }
+      print(s);
+      if (i%3 == 0){ print('Fizz'); } 
+    }
+  
+  
+  
+
+      
+      
